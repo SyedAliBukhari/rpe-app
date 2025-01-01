2bd145338ea17891ec8c5dee7ccb3efe92cb2172
admin <admin@admins-MacBook-Pro.local>
2025-01-01 23:46:19 +0000

    inital push

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1><br />
+        <p>This is my first app after almost 3 years</p>
+       </div>
+    </>
+  )
+}
+
+export default App
